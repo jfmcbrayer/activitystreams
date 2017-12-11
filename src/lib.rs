b5f7283ef5e65806fd592b9c6e84678c0fe0edf1
Jason McBrayer <jmcbray@carcosa.net>
2017-12-11 16:30:35 -0500

    Initial diesel stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,24 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#![recursion_limit="256"]
+#[macro_use] extern crate diesel_infer_schema;
+pub mod schema;
+pub mod models;
+
+#[macro_use] extern crate diesel;
+
+extern crate dotenv;
+
+
+use diesel::prelude::*;
+use diesel::pg::PgConnection;
+use dotenv::dotenv;
+use std::env;
+
+pub fn establish_connection() -> PgConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set");
+    PgConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url));
 }
+
